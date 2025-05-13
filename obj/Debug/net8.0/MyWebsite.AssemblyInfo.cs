d999c8a7e918af925126952ca2ac07bb14777247
Mikael Daskalou <mikael.daskalou@hotmail.com>
2025-05-13 10:16:14 +0200

    Initial commit with structured CSS and README

--- obj/Debug/net8.0/MyWebsite.AssemblyInfo.cs
+++ obj/Debug/net8.0/MyWebsite.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyWebsite")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+208f36221d3e12d8e01ce4e1f97f475f4013e70b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+380d59b23af92ca2427114e3f89f3aa16f286d32")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyWebsite")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyWebsite")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
